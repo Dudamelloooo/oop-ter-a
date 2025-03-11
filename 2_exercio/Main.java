1f1659af7d18fb24cf289cd746aa4027083980a4
John Doe <gmiello411@gmail.com>
2025-03-11 19:35:22 -0300

    Update Pessoa instances with modified attributes for demonstration

--- 2_exercio/Main.java
+++ 2_exercio/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa("João", 20, 1.80);
-        Pessoa pessoa2 = new Pessoa("Maria", 25, 1.70);
+        Pessoa pessoa1 = new Pessoa("João", 20, 1.70);
+        Pessoa pessoa2 = new Pessoa("Maria", 19, 1.75);
         
         pessoa1.exibirDados();
         pessoa2.exibirDados();
